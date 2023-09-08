f392154af2a55f7dcd0c8eff75f8d77026b203f7
eldpcn <cwiakowski.maciej@gmail.com>
2023-09-08 12:50:54 +0200

    updated command codes

---
+++ contracts/command_codes.go
@@ -0,0 +1,25 @@
+package iggcon
+
+const (
+	KillCode         = 0
+	PingCode         = 1
+	GetStatsCode     = 10
+	SendMessagesCode = 101
+	PollMessagesCode = 100
+	StoreOffsetCode  = 121
+	GetOffsetCode    = 120
+	GetStreamCode    = 200
+	GetStreamsCode   = 201
+	CreateStreamCode = 202
+	DeleteStreamCode = 203
+	GetTopicCode     = 300
+	GetTopicsCode    = 301
+	CreateTopicCode  = 302
+	DeleteTopicCode  = 303
+	GetGroupCode     = 600
+	GetGroupsCode    = 601
+	CreateGroupCode  = 602
+	DeleteGroupCode  = 603
+	JoinGroupCode    = 604
+	LeaveGroupCode   = 605
+)
