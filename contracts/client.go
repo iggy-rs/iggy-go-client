04e7950bd3b6f5e864401d6952af6f832b73b28b
eldpcn <cwiakowski.maciej@gmail.com>
2023-10-15 15:14:21 +0200

    add client operations for tcp protocol

--- contracts/client.go
+++ contracts/client.go
@@ -1,13 +1,10 @@
 package iggcon
 
 type ClientResponse struct {
-	ID                  uint                `json:"id"`
+	ID                  uint32              `json:"id"`
 	Address             string              `json:"address"`
-	UserID              uint                `json:"userId"`
+	UserID              uint32              `json:"userId"`
 	Transport           string              `json:"transport"`
-	ConsumerGroupsCount int                 `json:"consumerGroupsCount"`
-	ConsumerGroups      []ConsumerGroupInfo `json:"consumerGroups"`
-}
-
-type MeResponse struct {
+	ConsumerGroupsCount uint32              `json:"consumerGroupsCount"`
+	ConsumerGroups      []ConsumerGroupInfo `json:"consumerGroups,omitempty"`
 }
--- tcp/tcp_clients_managament.go
+++ tcp/tcp_clients_managament.go
@@ -1,20 +1,47 @@
 package tcp
 
 import (
+	binaryserialization "github.com/iggy-rs/iggy-go-client/binary_serialization"
 	. "github.com/iggy-rs/iggy-go-client/contracts"
 )
 
-func (tms *IggyTcpClient) GetMe() (*MeResponse, error) {
-	//TODO implement me
-	panic(GetMeCode)
-}
-
 func (tms *IggyTcpClient) GetClients() ([]ClientResponse, error) {
-	//TODO implement me
-	panic(GetClientsCode)
+	buffer, err := tms.sendAndFetchResponse([]byte{}, GetClientsCode)
+	if err != nil {
+		return nil, err
+	}
+
+	responseLength, err := getResponseLength(buffer)
+	if err != nil {
+		return nil, err
+	}
+
+	responseBuffer := make([]byte, responseLength)
+	_, err = tms.client.Read(responseBuffer)
+	if err != nil {
+		return nil, err
+	}
+
+	return binaryserialization.DeserializeClients(responseBuffer)
 }
 
 func (tms *IggyTcpClient) GetClientById(clientId int) (*ClientResponse, error) {
-	//TODO implement me
-	panic(GetClientCode)
+	message := binaryserialization.SerializeInt(clientId)
+	buffer, err := tms.sendAndFetchResponse(message, GetClientCode)
+	if err != nil {
+		return nil, err
+	}
+
+	responseLength, err := getResponseLength(buffer)
+	if err != nil {
+		return nil, err
+	}
+
+	responseBuffer := make([]byte, responseLength)
+	_, err = tms.client.Read(responseBuffer)
+	if err != nil {
+		return nil, err
+	}
+
+	return binaryserialization.DeserializeClient(responseBuffer), nil
 }
